25b755c59a6b4c5fa8fe3bc1c79eb947d9699a45
Thiago Leite <thiago.leite@agileengine.com>
2023-05-09 18:03:30 -0300

    Adding Home Page

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import { MeetupList } from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://blog.minhasinscricoes.com.br/wp-content/uploads/2022/05/blog-minhas-inscricoes-meetup.png",
+    address: "Some address 5, 12345",
+    description: "This is a first meetup.",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://blog.minhasinscricoes.com.br/wp-content/uploads/2022/05/blog-minhas-inscricoes-meetup.png",
+    address: "Some address 10, 12345",
+    description: "This is a second meetup.",
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS}/>;
+}
+
+export default HomePage;
