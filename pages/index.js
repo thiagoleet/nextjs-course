e18663312b5bff80e4384e75f7fb12ac19ebaa19
Thiago Leite <thiago.leite@agileengine.com>
2023-05-09 18:54:48 -0300

    Pre render content from Static Website

--- pages/index.js
+++ pages/index.js
@@ -19,8 +19,18 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+
+export async function getStaticProps() {
+  // fetch data from API
+
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
 }
 
 export default HomePage;
